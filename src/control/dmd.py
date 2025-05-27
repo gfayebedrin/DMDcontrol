125ce71bd6447a30d277922d606c187f5bd72dbe
Guillaume Faye-Bédrin <guillaume.faye-bedrin@sorbonne-universite.fr>
2025-05-27 18:43:36 +0200

    Basic DMD functions

--- src/control/dmd.py
+++ src/control/dmd.py
@@ -1,4 +1,5 @@
 from . import ALP4
+from functools import cached_property
 
 
 # Directory where the ALP-4.3 API is located
@@ -10,7 +11,22 @@ class DMD:
 
     def __init__(self):
         """Initialize the DMD device."""
-        self.alp4 = ALP4.ALP4(libDir=LIBDIR)
+        self._alp4 = ALP4.ALP4(libDir=LIBDIR)
+        self._alp4.Initialize()
+
+    # Properties
+
+    @property
+    def size(self):
+        """Size of the DMD in pixels."""
+        return self._alp4.nSizeX, self._alp4.nSizeY
+    
+    @cached_property
+    def max_sequence_length(self):
+        """Maximum length of a sequence."""
+        return self._alp4.DevInquire(ALP4.ALP_AVAIL_MEMORY)
+
+    # Dunder methods
 
     def __enter__(self):
         return self
@@ -18,11 +34,16 @@ class DMD:
     def __exit__(self, exc_type, exc_value, traceback):
         self.delete()
 
+    # Private methods
+
+    # Public methods
+
     def calibrate(self):
         pass
 
     def delete(self):
-        pass
+        self._alp4.Halt()
+        self._alp4.Free()
 
     def reset(self):
         pass
